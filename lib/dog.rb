3e47a57769f778b26ac61ee8d112e589ff20f5bc
romantabatha <romantabatha17@gmail.com>
2020-09-15 20:53:46 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+    def initialize(name,breed = "Mutt")
+        @name = name 
+        @breed = breed
+    end 
+
+    # def initialize(breed)
+    #     @breed = breed
+    # end
+   
+    # def breed=(breed)
+    #     @breed = breed
+    #     if breed =  " "
+    #         puts "Mutt"
+    #     end 
+    # end 
+
+    # def breed 
+    #     @breed
+    # end
+
+end 
