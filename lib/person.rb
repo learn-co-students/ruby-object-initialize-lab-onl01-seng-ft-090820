8bebb6cb471f4ae11a5e25ebbc0633616962224a
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 03:19:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,15 @@
 class Person
-  def initialize (persons_name)
-    @name = persons_name
+  def initialize(name)
+    @name = name
   end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
 
   
