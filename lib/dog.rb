e3dc9297e3a38e9326eec496356dfa7ac744e6b3
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 03:22:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,20 @@
 class Dog
-  def initialize(name, breed = "Mut")
-    @name = name
-    @breed = breed
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
 
-  def name=(name)
-    @name = name
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
 
-  def breed=(breed)
-    @breed = breed
-  end
-  
   def name
     @name
   end
-
   def breed
     @breed
-  end
-end
+  end 
+end 
+  
+  fido = Dog.new("Fido")
